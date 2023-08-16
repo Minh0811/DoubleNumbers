14bb1dafde450d8340e03e897c40694329cafded
minh0811 <vokhaiminh0811@gmail.com>
2023-08-16 19:59:29 +0700

    Initial Commit

---
+++ DoubleNumbers/DoubleNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleNumbersApp.swift
+//  DoubleNumbers
+//
+//  Created by Minh Vo on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DoubleNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
